6ba01109a06b46fe21bf987a96685baee687ee80
Franco Ignacio Villca <ignafrancov@gmail.com>
2024-02-10 21:54:58 -0300

    agregacion de tabla facturas

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -31,7 +31,6 @@ function clientes(){
     )
     .then(
         resp=>{
-            //class="fs-6 mt-3 p-0 btn btn-sm btn-link fw-bold text-decoration-none"
             document.getElementById('botonesrec').innerHTML='<button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#modalRegistroCliente">Agregar cliente</button>'
             document.getElementById("recurso").innerHTML = '<table id=tablacliente class="table table-hover table-sm"> </table>'
             var clientes = "<thead><tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Dni</th><th>Email</th><th>accion</th></tr></thead>"
@@ -194,4 +193,45 @@ function eliminar_cliente(id){
         }
     )
     .catch((error) => { console.log("Promesa rechazada por" , error)})
+}
+function facturas(){
+    const requestOptions={
+        method:'GET',
+        headers:{
+            'token-acceso' : token,
+            'id-usuario' : iduser
+        }
+    }
+    fetch(`http://127.0.0.1:5000/usuarios/${iduser}/factura`, requestOptions)
+    .then(
+        res =>{if (res.status === 200 || res.status === 400) {
+            return res.json();
+          } else {
+            throw new Error("Algo salió mal en el servidor API");
+          }}
+    )
+    .then(
+        resp=>{
+            document.getElementById('botonesrec').innerHTML='<button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#modalRegistrofactura">Crear Factura</button>'
+            document.getElementById("recurso").innerHTML = '<table id="tablafactura" class="table table-hover table-sm"> </table>'
+            var lista_facturas = "<thead><tr><th>ID</th><th>id cliente</th><th>fecha y hora</th><th>Productos</th><th>Descuento</th><th>total</th><th>acciones</th></tr></thead>"
+            if (resp.length === 0){
+                lista_facturas = lista_facturas.concat('<tr><td>No posee registros de clientes</td></tr>')
+            }
+            for(let i = 0; i < resp.length; i++){
+                let factura=`<tr id=${resp[i].id_factura}>
+                <td>${resp[i].id_factura}</td>
+                <td>${resp[i].id_cliente}</td>
+                <td>${resp[i].hora_fecha}</td>
+                <td>${resp[i].cant_productos}</td>
+                <td>${resp[i].descuento}</td>
+                <td>${resp[i].TOTAL}</td>
+                <td><button onclick=editar_cliente(${resp[i].id_factura}) class="btn btn-outline-secondary">Modificar</button>
+                <button onclick=eliminar_cliente(${resp[i].id_factura}) class="btn btn-outline-danger">Eliminar</button></td></tr>`
+                lista_facturas = lista_facturas.concat(factura)
+            }
+            document.getElementById("tablafactura").innerHTML=(lista_facturas)
+        }
+    )
+    .catch((error) => { console.log("Promesa rechazada por" , error)});
 }
